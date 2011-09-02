31bfebf0d222972bbc9e3e0d5e37cbdb4d25e060
JR-Fire <JR-Fire@192.168.1.103>
2011-09-02 21:56:59 +0300

    Separate the test in new folder

--- src/jia/begins/examples/basics/ogniana/rainova/common/IEmployee.java
+++ src/jia/begins/examples/basics/ogniana/rainova/common/IEmployee.java
@@ -2,20 +2,15 @@ package jia.begins.examples.basics.ogniana.rainova.common;
 
 import java.math.BigDecimal;
 
-/**
- * User: jr-fire
- * Date: Nov 6, 2010
- * Time: 7:43:41 AM
- */
 public interface IEmployee {
-    String getFirstName();
+	String getFirstName();
 
     void setFirstName(String m_firstName);
 
     String getLastName();
 
     void setLastName(String m_lastName);
-
+    
     int getAge();
 
     void setAge(int m_age);
